ec50162d95a54ae78c56e60e02375c8e9fad64bf
wuyuanqing <wuyuanqing4@huawei.com>
2024-11-13 09:57:02 +0800

    Remove better-escape.nvim and custom mini.surround keymap

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,4 @@
 -- 输入/跳转功能插件
-
 return {
     -- Markdown 表格模式
     {
@@ -9,30 +8,5 @@ return {
         end,
         cmd = { "TableModeToggle" },
         keys = { { "<leader>ut", "<cmd>TableModeToggle<cr>", desc = "Table mode" } },
-    },
-
-    -- 无延迟的使用jk退出Insert模式
-    {
-        "max397574/better-escape.nvim",
-        event = "InsertEnter",
-        config = function()
-            require("better_escape").setup()
-        end,
-    },
-
-    -- Change surrounding
-    {
-        "echasnovski/mini.surround",
-        opts = {
-            mappings = {
-                add = "gsa",
-                delete = "gsd",
-                find = "gsf",
-                find_left = "gsF",
-                highlight = "gsh",
-                replace = "gsc",
-                update_n_lines = "gsn",
-            },
-        },
     },
 }

7d2f86b1e8386fd1013c090b1f5b37e1d8c90469
wuyuanqing <wuyuanqing4@huawei.com>
2024-11-28 09:33:49 +0800

    Add lazy-lock.json

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -11,7 +11,7 @@ return {
     { -- set colorscheme
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "catppuccin",
+            colorscheme = "tokyonight",
         },
     },
 
